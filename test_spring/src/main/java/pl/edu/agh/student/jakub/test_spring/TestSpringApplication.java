d586d688ca16e9f8abd41f4fedb922f708f86d4b
JakubG-git <jakub.macius@gmail.com>
2022-10-16 20:47:55 +0200

    Student

--- test_spring/src/main/java/pl/edu/agh/student/jakub/test_spring/TestSpringApplication.java
+++ test_spring/src/main/java/pl/edu/agh/student/jakub/test_spring/TestSpringApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pl.edu.agh.student.jakub.test_spring.student.Student;
 
 import java.util.List;
 
@@ -15,8 +16,8 @@ public class TestSpringApplication {
 		SpringApplication.run(TestSpringApplication.class, args);
 	}
 	@GetMapping
-	public List<String> hello(){
-		return List.of("Hello", "World");
+	public List<Student> hello(){
+		return List.of(new Student());
 	}
 
 }
